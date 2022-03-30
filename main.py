d1c9d1325e4283b61674f00e4d1d84661e315382
Nikita <nikita.garanin.1999@mail.ru>
2022-03-30 17:28:24 +0300

    Редактирование

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from checkers.constants import X_SIZE, Y_SIZE, CELL_SIZE
 
 def main():
     main_window = Tk()
+    main_window.title('Bank')
     main_window.resizable(0, 0)
     main_window.iconphoto(False, PhotoImage(file='icon.png'))
 
