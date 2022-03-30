6491b5dd538cd172eb2e92a383e0d0d1a1a36d43
Nikita <nikita.garanin.1999@mail.ru>
2022-03-30 17:28:07 +0300

    Редактирование главного окна

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from checkers.game import Game
 from checkers.constants import X_SIZE, Y_SIZE, CELL_SIZE
 
 def main():
-    # Создание окна
     main_window = Tk()
-    main_window.title('Шашки')
     main_window.resizable(0, 0)
     main_window.iconphoto(False, PhotoImage(file='icon.png'))
 
